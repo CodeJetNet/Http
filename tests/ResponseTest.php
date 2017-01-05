3e4a83522746b29239844edf8b9bf5ccdd906df8
jhoughtelin <josh@findsomehelp.com>
2017-01-04 18:05:29 -0700

    Implement basic Response class and paired tests.

---
+++ tests/ResponseTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace CodeJet\Http;
+
+use Psr\Http\Message\ResponseInterface;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetStatusCode200WhenNoneIsSet()
+    {
+        $response = new Response();
+
+        $this->assertSame(200,$response->getStatusCode());
+    }
+
+    public function testGetEmptyReasonPhraseWhenNoneIsSet()
+    {
+        $response = new Response();
+
+        $reasonPhrase = $response->getReasonPhrase();
+
+        $this->assertTrue(is_string($reasonPhrase));
+        $this->assertTrue(empty($reasonPhrase));
+    }
+
+    public function testChangeStatusReturnsNewObject()
+    {
+        $response = new Response();
+
+        $responseWithNewStatus = $response->withStatus(200);
+
+        $this->assertInstanceOf(ResponseInterface::class, $responseWithNewStatus);
+        $this->assertNotSame($response, $responseWithNewStatus);
+    }
+
+    public function testCanChangeCodeAndPhrase()
+    {
+        $response = new Response();
+
+        $responseWithNewStatus = $response->withStatus(200, 'a sweet reason phrase.');
+
+        $this->assertSame(200, $responseWithNewStatus->getStatusCode());
+        $this->assertSame('a sweet reason phrase.', $responseWithNewStatus->getReasonPhrase());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testWithNonIntegerStatusCodeThrowsInvalidArgumentException()
+    {
+        $response = new Response();
+        $response->withStatus('This is not a 3 digit integer.');
+    }
+}
